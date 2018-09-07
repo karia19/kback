2671398a9b2f619e2364514d2e4ab8c8a027f1f5
Kari <kari@13-MacBook-Pro.local>
2018-09-07 20:36:11 +0300

    mm

--- models/persons.js
+++ models/persons.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose')
 
-require('dotenv').config()
-/*
+
+
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
   }
-  
-*/  
+   
 const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
